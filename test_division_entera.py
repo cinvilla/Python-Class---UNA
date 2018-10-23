dd8f6dcadb7f9713a58aa0c19836b4efc810f902
cinvilla <cylivralaj27@gmail.com>
2018-10-23 14:35:02 -0500

    Add files via upload

---
+++ test_division_entera.py
@@ -0,0 +1,13 @@
+def test_division_entera ():
+    num1 = 9
+    num2 = 3
+    myDiv = num1 // num2
+    print (myDiv)
+    assert 3 == myDiv
+
+def test_division_enterII ():
+    num3 =  20
+    num4 = 2
+    myDiv2 = num3 // num4
+    print (myDiv2)
+    assert (num4 + 8) == myDiv2
