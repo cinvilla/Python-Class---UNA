2951d8398a9a39b4ac10a137a31f1339c40960ec
cinvilla <cylivralaj27@gmail.com>
2018-10-28 10:10:18 -0600

    Dictionary test with print stataments

--- print_dictionary_test10282018.py
+++ print_dictionary_test10282018.py
@@ -8,11 +8,11 @@ print("This is my empty dict", my_empty_dict,
 dict_data['pedro']
 print()
 print(dict_data['pedro'], 'is Pedros phone number.',
-      'The [] brackets are used to display value assigned to keys'
-      'and to edit a value of a key I can simply set that key to another value using '
+      'The [] brackets are used to display value assigned to keys',
+      'and to edit a value of a key I can simply set the key to another value using '
       'the assignment operator =')
 
 # Para agregar keys dentro del diccionario usamos []
 dict_data['miguel'] = 48
 print()
-print('This shows dictionary update', dict_data)
+print('This shows a dictionary update', dict_data)
